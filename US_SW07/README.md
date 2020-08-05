2d344cd83612075ea7333d26554733136303b32f
Boyan Bejanov <bbejanov@bankofcanada.ca>
2020-08-05 14:51:35 -0400

    Steady state section almost done

--- US_SW07/README.md
+++ US_SW07/README.md
@@ -1,4 +1,11 @@
 # Smets and Wouters 2007
 
-Smets, F., Wouters, R., 2007. Shocks and frictions in US business cycles: A bayesian DSGE approach. The American Economic Review 97(3), 586–606.
+[Smets, F., Wouters, R., 2007. Shocks and frictions in US business cycles: A bayesian DSGE approach. The American Economic Review 97(3), 586–606.](https://www.aeaweb.org/articles?id=10.1257/aer.97.3.586)
+
+## Data
+
+The replication data can be downloaded from http://doi.org/10.3886/E116269V1
+You may need to create an account, if you don't already have one, download the zip file and extract its contents in the data/116269-V1/ directory.
+
+## 
 
--- US_SW07/main.jl
+++ US_SW07/main.jl
@@ -15,20 +15,86 @@ using ModelBaseEcon
 using StateSpaceEcon
 using TimeSeriesEcon
 
-## ###
+using  ModelBaseEcon: parameters, variables, shocks, equations
 
-# The model is in a module by the same name. 
-# Before we can load it (with `using`) we have to tell Julia where to find its file.
+## ##########################################################################
+
+# The model is in a module by the same name.
+# Before we can load it (with `using`) we have to tell Julia where to find it.
 unique!(push!(LOAD_PATH, mypath))
 using SW07
 m = SW07.model
 
-## ###
+## ##########################################################################
 
 # If the model is too big, you only see a summary.
 # To see the entire model, use `fullprint`
 fullprint(m)
 
-## #
+# We can also examine individual components of the model. For example
+variables(m)
+shocks(m)
+parameters(m)
+equations(m)
+
+# The model parameters can be accessed and even modified from the model
+# object using the dot notation.
+m.cmaw
+
+## ##########################################################################
+
+# Part 1: Solving the steady state problem. 
+
+# This needs to be done in order
+# to use the steady state for the final conditions.
+
+# Start by resetting any previous information about the steady state.
+# This is done with `clear_sstate!`. This is not really necessary, but
+# it helps to have this code here in case we come back to rerun the cell.
+clear_sstate!(m);
+
+# Next we solve for the steady state with a call to `ssolve!`
+sssolve!(m);
+
+# The steady state of the model is stored internally within the model instance.
+# This is why the two functions we called so far do not return any useful value,
+# rather they modify the steady state solution stored in `m`.
+
+# The steady state can be examined (and even modified) by accessing `m.sstate`.
+# Using dot notation one can read and write the current steady state values.
+m.sstate.a
+
+# At any point we can check whether the steady state solution stored in the model
+# object is in fact a steady state solution. This is done by calling
+# `check_sstate` It returns an `Int` equal to the number of steady state equations
+# that are violated.
+check_sstate(m) 
+
+# We want this number to be 0, although with non-linear models sometimes we
+# have truncation and round-off errors. We can control the accuracy of the
+# solution using the `tol` option in `sssolve!` above and the tolerance of the
+# check by passing the same option to `check_sstate`
+m.sstate.a = 1e-7
+check_sstate(m)
+check_sstate(m, tol=1e-6)
+
+
+# With the `verbose=true` option, the violated equations are printed,
+# sorted from the highest residuals at the top with decreasing residuals going down.
+check_sstate(m, tol=1e-7, verbose=true)
+
+# 
+m.sstate.a = 0.0
+check_sstate(m)
+
+# Finally, compare the steady state we obtained with the known steady state for this model.
+
+# TODO
+
+## ##########################################################################
+
+# Part 2: Impulse response.
+
+# TODO
 
 
