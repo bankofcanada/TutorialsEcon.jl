0d910d1a0e263bb50b83534b10d6e60dfcf4f95d
Boyan Bejanov <bbejanov@gmail.com>
2020-08-10 16:58:39 -0400

    copied code from md to jl file upto and including state space

--- src/US_SW07/main.jl
+++ src/US_SW07/main.jl
@@ -1,100 +1,182 @@
 
-#  if you copy-paste the following line into REPL it'll not work.
-#  This is what happens if you run it with Ctrl-Enter.
-
 # VSCode users:
 # Do not run next line of code with Ctrl-Enter!
 # Run next line of code with Alt-Enter (line), Shift-Enter (run cell), or Ctrl-F5 (run file)
 mypath = dirname(@__FILE__)
 
+## ##########################################################################
+# Initialization
+
 using Pkg
 Pkg.activate(joinpath(mypath, "..", ".."))
-
 
 using ModelBaseEcon
 using StateSpaceEcon
 using TimeSeriesEcon
 
-using  ModelBaseEcon: parameters, variables, shocks, equations
+# using  ModelBaseEcon: parameters, variables, shocks, equations
 
 ## ##########################################################################
+#### Part 1: The model
 
-# The model is in a module by the same name.
-# Before we can load it (with `using`) we have to tell Julia where to find it.
+### Load the model
+
+# The model is described in its own dedicated module, which in turn is contained
+# in its own file, `SW07.jl`. We can load the module with `using SW07`, then the
+# model itself is a global variable called `model` within that module.
 unique!(push!(LOAD_PATH, mypath))
 using SW07
 m = SW07.model
 
 ## ##########################################################################
+### Examine the model
 
-# If the model is too big, you only see a summary.
-# To see the entire model, use `fullprint`
+# This model is too big to fit all of its details in the REPL window, so only a
+# summary information is displayed. We can see the entire model with `fullprint`.
 fullprint(m)
 
-# We can also examine individual components of the model. For example
-variables(m)
-shocks(m)
+# We can also examine individual components using the commands `parameters`,
+# `variables`, `shocks`, `equations`.
 parameters(m)
-equations(m)
-
-# The model parameters can be accessed and even modified from the model
-# object using the dot notation.
-m.cmaw
 
 ## ##########################################################################
+### Setting the model parameters
 
-# Part 1: Solving the steady state problem. 
+# We must not change any part of the model in the running Julia session except for
+# the model parameters and [steady state constraints](@ref
+# steady_state_constraints). If we want to add variables, shocks, or equations, we
+# must do so in the model module file and restart Julia to load the new model.
+    
+# When it comes to the model parameters, we can access them by their names from
+# the model object using dot-notation.
+m.crr # read a parameter value
+m.cgy = 0.5187 # modify a parameter value
 
-# This needs to be done in order
-# to use the steady state for the final conditions.
+# !!! note 
+#     In this model the values of the parameters have been set according to the
+#     [`replication data`](@ref replication_data).
 
-# Start by resetting any previous information about the steady state.
-# This is done with `clear_sstate!`. This is not really necessary, but
-# it helps to have this code here in case we come back to rerun the cell.
-clear_sstate!(m);
+## ##########################################################################
+### The model flags and options
 
-# Next we solve for the steady state with a call to `ssolve!`
+# In addition to model parameters, which are values that appear in the model
+# equations, the model object also holds two other sets of parameters, namely
+# flags and options.
+
+# Flags are (usually boolean) values which characterize the type of model this is.
+# For example a linear model should have its `linear` flag set to `true`.
+# Typically this is done in the model file before calling [`@initialize`](@ref).
+m.flags
+
+# Options are values that adjust the operations of the algorithms. For example, we
+# have `tol` and `maxiter`, which set the desired accuracy and maximum number of
+# iterations for the iterative solvers. These can be adjusted as needed at any
+# time. Another useful option is `verbose`, which controls the verbosity.
+
+# Many functions in `StateSpaceEcon` have optional arguments by the same names as
+# the names of model options. When the argument is not explicitly given in the
+# function call, these functions will use the value from the model option of the
+# same name.
+m.verbose = true
+m.options
+
+
+## ##########################################################################
+#### Part 2: The Steady state solution
+
+# The steady state is a special solution of the dynamic system, one that remains
+# constant over time. It is important on its own, but also it can be useful in
+# several ways. One example is that linearizing the model requires a particular
+# solution about which to linearize and the steady state is the one typically used.
+
+# In addition to the steady state we also consider another kind of special
+# solution, which grows linearly in time. If we know that the steady state
+# solution is constant (i.e. its slope is zero), we can set the model flag
+# `ssZeroSlope` to `true`. This is not required, however in a large model it might
+# help the steady state solver converge faster to the solution.
+
+# The model object `m` stores information about the steady state. This includes
+# the steady state solution itself as well as a (possibly empty) set of additional
+# constraints that apply only to the steady state. This information can be
+# accessed via `m.sstate`.
+m.sstate
+
+## ##########################################################################
+### Steady state constraints
+
+# Sometimes the steady state is not unique, and one can use steady state
+# constraints to specify the particular steady state one wants. Also, if the model
+# is non-linear, these constraints can be used to help the steady state solver
+# converge. Steady state constraints can be added with the [`@steadystate`](@ref)
+# macro. It can be as simple as giving a specific value or we can write an
+# equation with multiple variables. We're allowed to use model parameters in these
+# equations as well.
+
+@steadystate m a = 5
+m.sstate
+
+# We can clean up the constraints by emptying the constraints container.
+
+empty!(m.sstate.constraints)
+m.sstate
+
+# !!! note "Important note"
+#     Steady state constraints that are always valid can be pre-defined in the model file.
+#     In that case, all calls to the [`@steadystate`](@ref) macro must be made after calling
+#    [`@initialize`](@ref).
+
+## ##########################################################################
+### Solve for the steady state
+
+# The steady state solution is stored within the model object. Before solving, we
+# have to specify an initial condition. If the model is linear, this makes no
+# difference, but in a non-linear model a good or a bad initial guess might be the
+# difference between success and failure of the steady state solver.
+
+# We can do this by calling [`clear_sstate!`](@ref). This call removes any
+# previously stored solution, sets the initial condition, and runs the pre-solve
+# pass of the steady state solver. The initial guess can be given with the `lvl`
+# and `slp` arguments; if not, an initial guess is chosen automatically.
+
+# Once that's done, we call [`sssolve!`](@ref) to find the steady state. This
+# function returns a `Vector{Float64}` containing the steady state solution, but
+# it also writes that solution into the model object. The vector is of length
+# `2*nvariables(m)` and contains the level and the slope for each variable.
+
+clear_sstate!(m)
 sssolve!(m);
 
-# The steady state of the model is stored internally within the model instance.
-# This is why the two functions we called so far do not return any useful value,
-# rather they modify the steady state solution stored in `m`.
+# If in doubt, we can use [`check_sstate`](@ref) to make sure the steady state solution
+# stored in the model object indeed satisfies the steady state system of equations.
+# This function returns the number of equations that are not satisfied.
+# A value of 0 is what we want to see. In verbose mode, the it also lists the
+# problematic equations and their residuals.
 
-# The steady state can be examined (and even modified) by accessing `m.sstate`.
-# Using dot notation one can read and write the current steady state values.
-m.sstate.a
-
-# At any point we can check whether the steady state solution stored in the model
-# object is in fact a steady state solution. This is done by calling
-# `check_sstate` It returns an `Int` equal to the number of steady state equations
-# that are violated.
-check_sstate(m) 
-
-# We want this number to be 0, although with non-linear models sometimes we
-# have truncation and round-off errors. We can control the accuracy of the
-# solution using the `tol` option in `sssolve!` above and the tolerance of the
-# check by passing the same option to `check_sstate`
-m.sstate.a = 1e-7
 check_sstate(m)
-check_sstate(m, tol=1e-6)
-
-
-# With the `verbose=true` option, the violated equations are printed,
-# sorted from the highest residuals at the top with decreasing residuals going down.
-check_sstate(m, tol=1e-7, verbose=true)
-
-# 
-m.sstate.a = 0.0
-check_sstate(m)
-
-# Finally, compare the steady state we obtained with the known steady state for this model.
-
-# TODO
 
 ## ##########################################################################
+### Examine the steady state
 
-# Part 2: Impulse response.
+# We can access the steady state solution via `m.sstate` using the dot notation.
+m.sstate.dc
 
-# TODO
+# We can also assign new values, but we should be careful to make sure it remains
+# a valid steady state solution.
+m.sstate.dc.slope = 0.001
+check_sstate(m)
+
+# Okay, let's undo that.
+m.sstate.dc.slope = 0
+check_sstate(m)
+
+# We can examine the entire steady state solution with [`printsstate`](@ref).
+printsstate(m)
+
+
+## ##########################################################################
+#### Part 3: Impulse response
+
+
+
 
 
